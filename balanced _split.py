79c483af02ef4ed88409b423f2135d58cf317411
nandupfactorial <nandus@pfactorial.ai>
2024-08-29 17:41:31 +0530

    removed unwanted code

--- balanced _split.py
+++ balanced _split.py
@@ -7,6 +7,7 @@ class paginate_base:
     def __init__(self,items,ls1,ls2):
         # page = page
         self.items = items
+        self.total_items = 2*self.items
         if self.items < 1:
             raise Exception('page shoud be greater than or equal to one')
         self.ls1 = ls1
@@ -17,9 +18,7 @@ class paginate_base:
         self.max_page2 = self.max_page(self.items,self.ls2len)
         self.max_page_limit = self.max_page1 if self.max_page1 < self.max_page2 else self.max_page2
         self.maxs1,self.maxe1,self.maxs2,self.maxe2 = self.get_balanced_indices(self.max_page_limit)
-        maxdiffe1s1 = self.maxe1 - self.maxs1
-        maxdiffe2s2 = self.maxe2 - self.maxs2
-        self.add_Factor = maxdiffe1s1-self.items if maxdiffe1s1 > maxdiffe2s2 else maxdiffe2s2 - self.items
+
 
         
     def get_indices(self,page,maxpage,llen):
@@ -51,26 +50,21 @@ class paginate_base:
         e1s1diff = e1-s1
         e2s2diff = e2 -s2
         if page <= self.max_page_limit:
+
             e1 = e1+items - e2s2diff
             e2 = e2+items - e1s1diff 
-            
         else:
+            mul_factor = page - self.max_page_limit if self.max_page_limit % 2 == 0 else page - self.max_page_limit -1
+
   
-            smult = self.max_page_limit+((page-self.max_page_limit-1)*2)
-            emult = smult+2
-            s,e = (smult*self.items)+self.add_Factor,(emult*self.items)+self.add_Factor
+            if self.get_indices(page,self.max_page1,self.ls1len) == (0,0) :
+                s2 = self.maxe2 + self.total_items*mul_factor
+                e2 = s2 + self.total_items
 
-            if e1s1diff == 0 :
-                if s >= self.ls2len and e >= self.ls2len:
-                    s2,e2 = 0,0
-                else:
-                    s2,e2 = s,e
+            elif self.get_indices(page,self.max_page2,self.ls2len) == (0,0):
+                s1 = self.maxe1 + self.total_items*mul_factor
+                e1 = s1 + self.total_items
 
-            elif e2s2diff == 0 :
-                if s >= self.ls1len and e >= self.ls1len:
-                    s1,e1 = 0,0       
-                else:
-                    s1,e1 = s,e
      
         return s1,e1,s2,e2
     
@@ -82,18 +76,21 @@ class paginate_base:
 
         return maxpage
 
-l1 = list(range(1))
-l2 = list(range(1))
+l1 = list(range(80))
+l2 = list(range(1000))
 
-start = time.time()
-a = paginate_base(1,l1,l2)
+# start = time.time()
+a = paginate_base(74,l2,l1)
+# end = time.time()
 
-print(a.get_balanced_indices(1))
+# print((end-start))
 
-end  = time.time()
+print(a.get_balanced_indices(2),a.max_page_limit)
 
-print(a.__dict__)
+
+
+# print(a.__dict__)
 # print(a.max_page_limit)
 # print(a.add_Factor)
 
-    
+# [1,2,3,4,,,5,6,7,8],[1,2,3,4,,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24]
