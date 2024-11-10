baced5dade73585383756021d42c833d9e5a0d7c
Nandu  s <74356710+Nandusasikumar1@users.noreply.github.com>
2024-11-11 00:29:47 +0530

    Update balanced _split.py

--- balanced _split.py
+++ balanced _split.py
@@ -77,3 +77,31 @@ class paginate_base:
 
         return maxpage
 
+
+
+
+c = {1:{4:5,7:{8:9}}}
+def search(d,key):
+    temp = {}
+    endcheck = []
+    while True:
+        if key in d:
+            return {key:d[key]}
+        for k,v in d.items():
+            if isinstance(v,dict):
+                temp |= v
+                endcheck.append(False)
+            else:
+                endcheck.append(True)
+        if temp:
+            d,temp = temp,{}
+        if all(endcheck) and key not in temp:
+            return 'key not found'
+        
+        endcheck = []
+
+print(search(c,7))
+                
+            
+        
+
